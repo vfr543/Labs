fc94a2ce4719c9e9804d333e6181d03c3995c58c
vfr543 <127236384+vfr543@users.noreply.github.com>
2025-09-20 08:56:03 +0300

    Update Task2.cpp

--- Lab_inf_1.1/Task2.cpp
+++ Lab_inf_1.1/Task2.cpp
@@ -2,14 +2,15 @@
 
 int main() {
     int stop, sum = 0;
-    setlocale(LC_ALL, "");
-    std::wcout << L"Введите количество остановок: ";
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Введите количество остановок: ";
     std::cin >> stop;
     for (int i = 0; i < stop; i++) {
         sum += 5;
     }
-    std::wcout << L"Стоимость проезда: ";
+    std::cout << "Стоимость проезда: ";
     std::cout << sum << std::endl;
     return 0;
 
 }
+
