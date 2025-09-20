d1ee2a20fdefcba61252d41e865ca6c8ba108c69
vfr543 <127236384+vfr543@users.noreply.github.com>
2025-09-20 08:39:13 +0300

    Update Task1.cpp

--- Lab_OaIP_1/Task1.cpp
+++ Lab_OaIP_1/Task1.cpp
@@ -2,35 +2,35 @@
 int minEl(int a[], int n);
 int main() {
 	int mas[10], sum = 0;
-	setlocale(LC_ALL, "Rus");
+	setlocale(LC_ALL, "");
 
 
 	for (int i = 0; i < 10; i++) {
-		std::cout << L"Введите элемент " << i + 1 << ":";
+		std::wcout << L"Введите элемент " << i + 1 << ":";
 		std::cin >> mas[i];
 		sum += mas[i];
 	}
-	std::cout << L"Массив: ";
+	std::wcout << L"Массив: ";
 	for (int i = 0; i < 10; i++) {
 		std::cout << mas[i] << ' ';
 	}
-	std::cout << std::endl << L"Сумма: " << sum << std::endl;
-	std::cout << L"Минимальный элемент: " << minEl(mas, 0) << std::endl;
-	for (int i = 0; i < 10; i++) {
+	std::wcout << std::endl << L"Сумма: " << sum << std::endl;
+	std::wcout << L"Минимальный элемент: " << minEl(mas, 0) << std::endl;
+	for (int i = 0; i < 9; i++) {
 		for (int j = i; j < 9; j++) {
 			if (minEl(mas, i) == mas[j + 1]) {
 				std::swap(mas[i], mas[j + 1]);
 			}
 		}
 	}
-	std::cout << L"Отсортированный массив: ";
+	std::wcout << L"Отсортированный массив: ";
 	for (int i = 0; i < 10; i++) {
 		std::cout << mas[i] << ' ';
 	}
 	return 0;
 }
 int minEl(int a[], int n) {
-	int min = a[0];
+	int min = a[n];
 	for (int i = n; i < 10; i++) {
 		if (a[i] < min) {
 			min = a[i];
@@ -39,5 +39,4 @@ int minEl(int a[], int n) {
 	return min;
 
 }
-
 
