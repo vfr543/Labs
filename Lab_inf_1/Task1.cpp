0750740542ff0afdbad3a611b0364b0ac11de8f7
vfr543 <127236384+vfr543@users.noreply.github.com>
2025-09-20 08:40:29 +0300

    Update Task1.cpp

--- Lab_inf_1/Task1.cpp
+++ Lab_inf_1/Task1.cpp
@@ -3,21 +3,22 @@
 int main()
 {
     double t;
-    setlocale(LC_ALL, "Rus");
-    std::cout << L"Введите температуру: ";
+    setlocale(LC_ALL, "");
+    std::wcout << L"Введите температуру: ";
     std::cin >> t;
     if (t < 0) {
-        std::cout << L"Наденьте зимнюю одежду" << std::endl;
+        std::wcout << L"Наденьте зимнюю одежду" << std::endl;
     }
     else if (t >= 0 && t <= 10) {
-        std::cout << L"Наденьте тёплую одежду" << std::endl;
+        std::wcout << L"Наденьте тёплую одежду" << std::endl;
     }
     else if (t > 10 && t <= 20) {
-        std::cout << L"Наденьте лёгкую одежду" << std::endl;
+        std::wcout << L"Наденьте лёгкую одежду" << std::endl;
     }
     else {
-        std::cout << L"Наденьте летнюю одежду" << std::endl;
+        std::wcout << L"Наденьте летнюю одежду" << std::endl;
     }
     return 0;
 
 }
+
